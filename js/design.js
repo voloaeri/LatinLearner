43a2664e23f060d5887d7083704fef05927d8b5a
Hannah Kyung <mime.is.k@gmail.com>
2015-09-26 22:32:04 -0400

    ADDED DESIGN
    STUFF

--- js/design.js
+++ js/design.js
@@ -1,1 +1,53 @@
 // hannah's first line
+
+// $(document).mouseover(function(){
+//   $('#title').fadeOut(1000);
+//   $('#nav1').fadeOut(2000);
+// })
+
+$(document).ready(function(){
+  $("button").click(function(){
+    $(".nav").hide("slow")
+    // ,
+    // function(){
+      // alert("This paragraph is now hidden.");
+    // });
+  });
+});
+// 
+// $(document).ready(function(){
+//   $("button").click(function(){
+//     $(".nav").show("slow")
+//     // , function(){
+//     //   alert("This paragraph is now shown.");
+//     // });
+//   });
+// });
+
+
+
+// $(document).hover(function () {
+//   $('#title').fadeOut(1000);
+//   $("#nav1").hover({
+
+
+// $('#nav1').mouseout(function(){
+//   $('#nav1').fadeIn(1000);
+// });
+
+
+//
+// $('#noun').hover(function(){
+//     $(this).css('color', 'pink');
+//   $(this).text('HANNAH IS BOOTYFUL');
+// });
+//
+//
+//
+// hello("testing");
+//
+// function hello(foo){
+//   var name = 'hannah is cute';
+// console.log(foo);
+// name = 5;
+// }
