610076a75d625d3cb46fad7db153be1253060457
Hannah Kyung <mime.is.k@gmail.com>
2015-09-27 11:28:02 -0400

    SUPER CHANGE

--- js/design.js
+++ js/design.js
@@ -1,5 +1,5 @@
 // hannah's first line
-
+// THIS IS THE HEADER
 // $('#top').mouseenter(function(){
 //   $('#title').fadeIn(500);
 // });
@@ -7,11 +7,11 @@
 // $('#top').mouseleave(function(){
 //   $('#title').fadeOut(500);
 // });
-
+// FOR THE DIFFERENT HEADER
 var black = true;
 $(document).ready(function(){
   $('#title').fadeOut(2000);
-  $("#top").hover(function(){
+  $('#top').hover(function(){
     if(black == true){
       $('#title').fadeIn(700);
       black = false;
@@ -22,7 +22,7 @@ $(document).ready(function(){
     };
   });
 });
-
+// THIS IS THE MENU
 var menu = true;
 $(document).ready(function(){
   $('.item').fadeOut(2000);
@@ -34,6 +34,43 @@ $(document).ready(function(){
     else {
       $('.item').fadeOut(700);
       menu = true;
+    };
+  });
+});
+// THESE ARE THE BUTTONS
+function changeDark(){
+  document.body.style.background = '#202020';
+  document.getElementById("title").style.color = 'linen';
+  document.getElementById("subject").style.color = 'linen';
+  document.getElementById("noun").style.color = 'linen';
+  document.getElementById("foot").style.color = 'linen';
+  document.getElementById("van").style.color = 'linen';
+  document.getElementById("anv").style.color = 'linen';
+  document.getElementById("avn").style.color = 'linen';
+  // document.getElementsByClassName('nav').style.color = "linen";
+};
+function changeLight(color){
+  document.body.style.background = 'linen';
+  document.getElementById("title").style.color = '#202020';
+  document.getElementById("subject").style.color = '#202020';
+  document.getElementById("noun").style.color = '#202020';
+  document.getElementById("foot").style.color = '#202020';
+  document.getElementById("van").style.color = '202020';
+  document.getElementById("anv").style.color = '202020';
+  document.getElementById("avn").style.color = '202020';
+};
+// THIS IS THE FOOTER
+var food = true;
+$(document).ready(function(){
+  $('mother').fadeOut(2000);
+  $('#foot').hover(function(){
+    if(food == true){
+      $('#foot').fadeIn(700);
+      food = false;
+    }
+    else {
+      $('#foot').fadeOut(700);
+      food = true;
     };
   });
 });
