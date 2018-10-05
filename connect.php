1a6b74e39a8e23b534c9cebdca2ad21dca7f7674
Volodymyr Siedlecki <volosied@gmail.com>
2018-10-05 12:22:44 -0400

    Update connect.php

--- connect.php
+++ connect.php
@@ -2,9 +2,9 @@
 ini_set('display_errors',1);
  error_reporting(E_ALL);
  
- $servername = "45.55.52.107";
- $username = "root";
- $password = "lDragon5";
+ $servername = "";
+ $username = "";
+ $password = "";
  $dbname = "nouns";
  // Create connection
  $conn = new mysqli($servername, $username, $password, $dbname);
